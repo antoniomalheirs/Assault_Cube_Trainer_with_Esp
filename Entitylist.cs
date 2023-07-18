f5f62703579f30469c87d316fcbcd4be96954858
antoniomalheirs <antoniomalhe@gmail.com>
2023-07-18 01:22:30 -0300

    definindo classes/ implementação função de para listar entidades

---
+++ Entitylist.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esp_Hack
+{
+    internal class Entitylist
+    {
+    }
+}
