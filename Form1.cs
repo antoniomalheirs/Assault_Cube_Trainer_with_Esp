a193b4951b3340cd486ab98797891d34f47932c9
antoniomalheirs <antoniomalhe@gmail.com>
2023-07-17 23:04:13 -0300

    first commit

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace Esp_Hack
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+namespace Esp_Hack
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+
+
+        }
+    }
+}
